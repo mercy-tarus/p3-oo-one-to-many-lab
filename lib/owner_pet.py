3af9254fbec3aebd9f0d752f74c68b81f5641cd4
mercy-tarus <merciechelah1@gmail.com>
2024-06-07 23:21:13 +0300

    complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,50 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    # GETTER / READER
+    @property
+    def pet_type(self):
+        print("GETTING _pet_type")
+        return self._pet_type
+    
+    # SETTER / WRITER
+    @pet_type.setter
+    def pet_type(self, new_pet_type):
+        print("SETTING _pet_type")
+        Hell = Exception
+        if new_pet_type not in Pet.PET_TYPES:
+            raise Hell(f"{new_pet_type} not in pet types")
+        else:
+            self._pet_type = new_pet_type
+
 
 class Owner:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [ pet for pet in Pet.all if pet.owner == self ]
+    
+    def add_pet(self, new_pet):
+        if isinstance(new_pet, Pet):
+            new_pet.owner = self
+        else:
+            raise Exception("NO")
+    
+    def get_sorted_pets(self):
+        # GET MY PETS
+        my_pets = self.pets()
+        # SORT MY PETS
+        sorted_pets = sorted( my_pets, key = lambda each_pet: each_pet.name.lower() )
+        # RETURN EM
+        return sorted_pets
